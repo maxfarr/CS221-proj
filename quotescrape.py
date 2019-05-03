6545c4a9aa2ddb99e04a28c9e700121684b55f8b
Valerie Garcia <valerie.garcia01@gmail.com>
2019-05-03 01:04:57 -0700

    for tag in t:         possible_tags.add(tag)

---
+++ quotescrape.py
@@ -0,0 +1,48 @@
+# import libraries
+import numpy as np
+import math
+import collections
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = "https://www.goodreads.com/quotes/tag/{}?page={}"
+
+num = 1;
+list_of_df = []
+for tag in possible_tags:
+    page = requests.get(url.format(tag, 1))
+    soup = BeautifulSoup(page.text, 'html.parser')
+    container = soup.body.find(class_="mainContentFloat").find(class_ = "leftContainer")
+    if(container.find("div", attrs={"style": "float: right;"}).div == None):
+        lastpage = 1
+    else:
+        lastpage = container.find("div", attrs={"style": "float: right;"}).div.contents[-3].contents[0]
+        lastpage = int(lastpage)
+    print("{}, {}: {}, {} pages".format(num, float(num) / len(possible_tags), tag, lastpage))
+    num += 1
+    quotefeatures = []
+    for i in range(lastpage):
+        page = requests.get(url.format(tag, i))
+        soup = BeautifulSoup(page.text, 'html.parser')
+        container = soup.body.find(class_="mainContentFloat").find(class_ = "leftContainer")
+        quotes = container.find(class_="quotes")
+        if(quotes == None):
+            continue
+        quotes = quotes.find_all(class_="quote")
+        for quote in quotes:
+            text = quote.find("div").find("div").contents[0].strip()[1:-1]
+            author = quote.find("div").find("div").find("span").contents[0].strip()
+            likes = quote.find("div").find(attrs={"class": "quoteFooter"}).\
+                    find("div", attrs={"class", "right"}).find("a").contents[0][:-6]
+            likes = int(likes)
+            tags = quote.find("div").find(attrs={"class": "quoteFooter"}).find("div").find_all("a")
+            taglist = [tag.contents[0] for tag in tags]
+            features = {"text": text, "author": author, "likes": likes, "tags": taglist}
+            quotefeatures.append(features)
+    list_of_df.append(pd.DataFrame(quotefeatures))
+        
+
+
+
+
