eeb3604ed9fddb4b56a156eb8aeff5a63edaaf0d
Valerie Garcia <valerie.garcia01@gmail.com>
2019-05-03 02:14:21 -0700

    oracle just gets a random quote

--- oracle.py
+++ oracle.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+if name == "__main__":
+    df = pd.read_csv("popular_quotes.csv")
+    df = df.drop('Unnamed: 0', 1)
 
-df = pd.read_csv("popular_quotes.csv")
-df = df.drop('Unnamed: 0', 1)
-
-quotes = df.text
+    quotes = df.text.tolist()
+    quote = random.choice(quotes)
+    print(quote)
