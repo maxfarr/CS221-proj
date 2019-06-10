6e49cfe7cc5a83aa0c981ac73f5eebf05d10de5e
Valerie Garcia <valerie.garcia01@gmail.com>
2019-06-10 12:26:53 -0700

    clean taggeed data

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import string
+import os
+
+PATH = "INSERT PATH HERE"
 
 def clean_csv(csv_file):
     df = pd.read_csv(csv_file)
@@ -19,9 +22,16 @@ def clean_csv(csv_file):
     mask = df["text"].apply(fn)
     df = df[mask]
     df = df.groupby("text").first().reset_index()[["author", "text", "likes", "tags"]]
-    df = df.to_csv("{}_clean.csv".format(csv_file[:-4]), index=False)
+    return df
+    #df = df.to_csv("{}_clean.csv".format(csv_file[:-4]), index=False)
 
 def create_corpus(csv):
     clean = pd.read_csv("popular_quotes_clean.csv")
     corpus = [x.split() for x in clean["text"].astype(str).tolist()]
     corpus = set([x for sublist in corpus for x in sublist])
+
+df_list = []
+for filename in os.listdir(path):
+    df_list.append(clean_csv(filename))
+df_big = pd.concat(df_list)
+df_big.to_csv("full_quotes_clean.csv", index=False)
